7b50b77a4d1437f7ca9a2bd4a535fd3bb1878c61
TimurPuchinsky <timur.2.puchinsky@gmail.com>
2023-04-28 10:33:01 +0300

    обавлены окна регистрации и входа в учетную запись. Создано БД для сохранения данных

--- app/src/main/java/com/example/gpshelp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/gpshelp/fragments/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.gpshelp.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +9,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.gpshelp.R;
+import com.example.gpshelp.activitys.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class ProfileFragment extends Fragment {
 
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button mainButton = view.findViewById(R.id.buttonRedact);
+        auth = FirebaseAuth.getInstance();
+        button = view.findViewById(R.id.buttonChangeUser);
+        textView = view.findViewById(R.id.textView10);
+        user = auth.getCurrentUser();
+        if (user == null){
+            Intent i = new Intent(getActivity(), LoginActivity.class);
+            startActivity(i);
+            ((Activity) getActivity()).overridePendingTransition(0, 0);
+        } else {
+            textView.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(getActivity(), LoginActivity.class);
+                startActivity(i);
+                ((Activity) getActivity()).overridePendingTransition(0, 0);
+            }
+        });
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainFragment mainFragment = new MainFragment();
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, mainFragment).commit();
+            }
+        });
+        return view;
     }
 }
