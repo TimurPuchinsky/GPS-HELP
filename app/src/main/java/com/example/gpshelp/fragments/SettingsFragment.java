aeb3455aa638bd5a77401ee233a8fd0ee8581425
TimurPuchinsky <timur.2.puchinsky@gmail.com>
2023-05-09 13:25:02 +0300

    Добавлена настройка для смены фона приложения

--- app/src/main/java/com/example/gpshelp/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/gpshelp/fragments/SettingsFragment.java
@@ -1,21 +1,58 @@
 package com.example.gpshelp.fragments;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import com.example.gpshelp.R;
 
 
 public class SettingsFragment extends Fragment {
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch switcher;
+    boolean nightmode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        switcher = view.findViewById(R.id.switch3);
+        sharedPreferences = requireContext().getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightmode = sharedPreferences.getBoolean("night", false);
+
+        if (nightmode){
+            switcher.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        switcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nightmode){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", false);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", true);
+                }
+                editor.apply();
+            }
+        });
+        return view;
     }
 }
